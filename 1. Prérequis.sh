f8d037c8e023f2b54677aa19aaaca88ef49e6518
TheRake66 <none>
2026-07-25 03:14:35 +0200

    details

--- 1. Prérequis.sh
+++ 1. Prérequis.sh
@@ -1,5 +1,7 @@
-# Configuration des redirections du nom de domaine.
+# Configuration du VPS.
 Debian 13
+
+# Configuration des redirections du nom de domaine.
 matrix.example.com       (A)     ---> IPv4
 matrix.example.com       (AAAA)  ---> IPv6
 element.example.com      (CNAME) ---> matrix.example.com
